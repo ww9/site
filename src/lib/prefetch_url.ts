6e7581c8a471d5893ff56a9bb994de185e253420
ww9 <ww9nine9@gmail.com>
2021-12-30 04:48:06 -0400

    added TypeScript types for prefetchUrl()

--- src/lib/prefetch_url.ts
+++ src/lib/prefetch_url.ts
@@ -3,8 +3,8 @@ import { prefetch } from '$app/navigation';
 // Since sveltekit:prefetch didn't work for me, I made my own.
 // https://github.com/sveltejs/kit/issues/2929
 // We can't really use on:click|once here because focus and click could trigger fetching the same content twice.
-export const prefetchUrl = function (e) {
-	const link = e.target;
+export const prefetchUrl = function (e: Event): void {
+	const link = <HTMLElement>e.target;
 	if (link.getAttribute('fetched')) return;
 	const url = link.getAttribute('href');
 	prefetch(url);
